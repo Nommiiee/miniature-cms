548fb57da84ab7f4a8b179cba0192efdcc55042d
Nommiiee <kunalkashyp3@gmail.com>
2023-02-27 10:58:06 +0530

    started working on Authorisation on FE

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useState } from "react";
+
+export default function login() {
+  return (
+    <>
+      <div className="w-full h-screen"></div>
+    </>
+  );
+}
